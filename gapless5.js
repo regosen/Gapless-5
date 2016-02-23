aebb1ad0c4ad73424087b7fd6b4dbf2ad3c52cea
Justin Cassidy <boil.afraid@gmail.com>
2016-02-22 22:10:16 -0800

    unshuffle to the correct current track

--- gapless5.js
+++ gapless5.js
@@ -425,8 +425,15 @@ var Gapless5FileList = function(inPlayList, inStartingTrack) {
 			that.previous = that.current;
 			// TODO: Find where current song is in original playlist, and make that
 			// the head of the new unshuffled playlist
-			// var track = that.current[that.currentItem].name;
-			that.current = reorderPlayList(that.original, that.currentItem);
+			var track = that.current[that.currentItem].name;
+	                for (var i = 0; i < that.original.length ; i++ )
+			{
+				if (track == that.original[i].name )
+				{
+					that.currentItem = i;
+                         		that.current = reorderPlayList(that.original, that.currentItem);
+				}
+                        }
 			shuffleMode = false;
 		}
 		that.currentItem = 0;	// Position to head of list
