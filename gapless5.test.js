2c16e58eb770a4a2ad325602c3d294eb3504da14
Rego Sen <regosen@gmail.com>
2022-05-20 09:36:07 -0600

    expand unit test coverage

--- gapless5.test.js
+++ gapless5.test.js
@@ -1,31 +1,37 @@
-// Mocks
-window = {
-  clearTimeout     : jest.fn(),
-  setTimeout       : jest.fn(),
+const { Gapless5, LogLevel } = require('./gapless5.js');
+
+const mockAPIs = () => {
+  const xhrMockClass = () => ({
+    open            : jest.fn(),
+    send            : jest.fn(),
+    setRequestHeader: jest.fn(),
+    abort           : jest.fn(),
+  });
+  XMLHttpRequest = jest.fn().mockImplementation(xhrMockClass);
+
+  const audioMockClass = () => ({
+    addEventListener: jest.fn(),
+    load: jest.fn(),
+    pause: jest.fn(),
+    play: jest.fn(() => Promise.resolve(jest.fn())),
+  });
+  Audio = jest.fn().mockImplementation(audioMockClass);
+
+  const audioContextMockClass = () => ({
+    createGain: () => ({
+      gain: { value: 1 },
+      connect: jest.fn(),
+    }),
+  });
+
+  window = {
+    clearTimeout     : jest.fn(),
+    setTimeout       : jest.fn(),
+    AudioContext     : jest.fn().mockImplementation(audioContextMockClass),
+  };
 };
 
-const xhrMockClass = () => ({
-  open            : jest.fn(),
-  send            : jest.fn(),
-  setRequestHeader: jest.fn(),
-  abort           : jest.fn(),
-});
-XMLHttpRequest = jest.fn().mockImplementation(xhrMockClass);
-
-const audioMockClass = () => ({
-  addEventListener: jest.fn(),
-  load: jest.fn(),
-  pause: jest.fn(),
-  play: jest.fn(() => Promise.resolve(jest.fn())),
-});
-Audio = jest.fn().mockImplementation(audioMockClass);
-
-const audioContextMockClass = () => ({
-  createGain: jest.fn(),
-});
-AudioContext = jest.fn().mockImplementation(audioContextMockClass);
-
-const { Gapless5, LogLevel } = require('./gapless5.js');
+/** test data **/
 
 const TRACKS = [ '0.mp3', '1.mp3', '2.mp3', '3.mp3', '4.mp3' ];
 const INIT_OPTIONS = {
@@ -34,8 +40,15 @@ const INIT_OPTIONS = {
   useHTML5Audio: false,
 };
 
+/** test suite **/
+
 describe('Gapless-5 object', () => {
   let player = null;
+
+  beforeAll(() => {
+    mockAPIs();
+  });
+
   beforeEach(() => {
     player = new Gapless5(INIT_OPTIONS);
   });
@@ -44,26 +57,38 @@ describe('Gapless-5 object', () => {
     expect(player.getTracks()).toStrictEqual([]);
     expect(player.totalTracks()).toBe(0);
     expect(player.getIndex()).toBe(-1);
+    expect(player.getPosition()).toBe(0);
     expect(player.canShuffle()).toBe(false);
     expect(player.initialized).toBe(true);
     expect(player.hasGUI).toBe(false);
   });
 
-  it('can manipulate tracklist', () => {
+  it('can manipulate tracklist and position', () => {
     player.addTrack(TRACKS[1]);
     expect(player.getIndex()).toBe(0);
     player.insertTrack(0, TRACKS[0]);
     expect(player.getTracks()).toStrictEqual([ TRACKS[0], TRACKS[1] ]);
     expect(player.findTrack(TRACKS[1])).toBe(1);
+    player.setPlaybackRate(0.1);
+    player.setPosition(10);
+    expect(player.getPosition()).toBe(10);
+
     player.removeTrack(0);
     expect(player.getTracks()).toStrictEqual([ TRACKS[1] ]);
     player.removeAllTracks();
     expect(player.getTracks()).toStrictEqual([]);
+    player.setPosition(10);
+    expect(player.getPosition()).toBe(0); // setPosition should do nothing without a track
   });
 });
 
 describe('Gapless-5 object with tracklist', () => {
   let player = null;
+
+  beforeAll(() => {
+    mockAPIs();
+  });
+
   beforeEach(() => {
     player = new Gapless5({
       ...INIT_OPTIONS,
@@ -74,6 +99,7 @@ describe('Gapless-5 object with tracklist', () => {
   it('has correct initial state', () => {
     expect(player.getTracks()).toStrictEqual(TRACKS);
     expect(player.getIndex()).toBe(0);
+    expect(player.getPosition()).toBe(0);
     expect(player.initialized).toBe(true);
     expect(player.hasGUI).toBe(false);
   });
@@ -139,6 +165,10 @@ describe('Gapless-5 object with tracklist', () => {
 });
 
 describe('Gapless-5 object with load limit', () => {
+  beforeAll(() => {
+    mockAPIs();
+  });
+
   it('obeys load limit', () => {
     const player = new Gapless5({
       ...INIT_OPTIONS,
